845285e42dced6988f8061e813de7e16d530da8e
Diyorbek Sultanov <diyorbeksultanov.ss@gmail.com>
2026-05-02 21:47:15 +0500

    Add quiz and mini test features to bot settings

--- bot.py
+++ bot.py
@@ -112,6 +112,8 @@ async def start_handler(message: Message):
     stats_enabled = get_setting("stats_enabled", "True")
     chatbot_enabled = get_setting("chatbot_enabled", "True")
     mock_enabled = get_setting("mock_enabled", "True")
+    quiz_enabled = get_setting("quiz_enabled", "True")
+    mini_test_enabled = get_setting("mini_test_enabled", "True")
 
     await message.answer(
         "👋 <b>Assalomu alaykum! Bo'stonliq tuman ixtisoslashtirilgan maktabining DTM Natijalar Botiga xush kelibsiz!</b>\n\n"
@@ -124,7 +126,7 @@ async def start_handler(message: Message):
         "🔗 <b>Profilni ulash:</b>\n"
         "Avtomatik xabarnoma uchun <code>ULASH_KODINGIZ</code> deb yozing.",
         parse_mode="HTML",
-        reply_markup=user_menu_keyboard(ranking_enabled, stats_enabled, chatbot_enabled, mock_enabled),
+        reply_markup=user_menu_keyboard(ranking_enabled, stats_enabled, chatbot_enabled, mock_enabled, quiz_enabled, mini_test_enabled),
     )
 
 
